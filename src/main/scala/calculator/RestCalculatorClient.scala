ccfbf5bbe110a70660081397969646196dda14cc
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-05-01 23:50:51 -0500

    Added Rest request client

--- src/main/scala/calculator/RestCalculatorClient.scala
+++ src/main/scala/calculator/RestCalculatorClient.scala
@@ -1,5 +1,19 @@
 package calculator
 
-object RestCalculatorClient extends App{
+import scalaj.http.Http
 
+object RestCalculatorClient extends App {
+  println("Making Get Request .... ")
+
+
+  val result = Http("https://a4r9djxhra.execute-api.us-east-1.amazonaws.com/default/CalculatorLambda").param("op1", "10").
+    param("op2", "30").
+    param("operator", "+")
+    .header("x-api-key", "QDqkEgvLZZ2rqt5RlZ6Gx2SpOm0FsFRE5yjsByQZ").asString
+
+
+  
+  println("Response form the lambda function ")
+
+  println(result.body)
 }
