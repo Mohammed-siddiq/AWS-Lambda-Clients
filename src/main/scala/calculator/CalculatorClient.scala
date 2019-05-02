c957f2ad8dd7ba9c570ca89b92146221619b8ed0
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-05-01 22:39:02 -0500

    Modified proto to support errors

--- src/main/scala/calculator/CalculatorClient.scala
+++ src/main/scala/calculator/CalculatorClient.scala
@@ -1,26 +1,31 @@
 package calculator
 
+
 import io.grpc.calculator.{Input, Response}
 import scalaj.http.{Base64, Http}
-
-
 
 
 object CalculatorClient extends App {
   //  Http("https://a4r9djxhra.execute-api.us-east-1.amazonaws.com/default/FirstLambda").
 
-  val data = Input(10, 20, "+")
+  println("Sending Request Proto .... ")
+
+  val data = Input(10, 20, "/")
+
+  println(data.toString)
+
   val result = Http("https://a4r9djxhra.execute-api.us-east-1.amazonaws.com/default/CalculatorLambda").postData(data.toByteArray)
     .header("Content-Type", "application/grpc+proto")
     .header("x-api-key", "QDqkEgvLZZ2rqt5RlZ6Gx2SpOm0FsFRE5yjsByQZ").asString
 
+
   val raw_body = Base64.decode(result.body)
-  println(result.headers)
 
   val returnedResponse = Response.parseFrom(raw_body)
 
-  println(returnedResponse.toString)
+  println("Response form the lambda function ")
 
+  println(returnedResponse.toString)
 
 
 }
