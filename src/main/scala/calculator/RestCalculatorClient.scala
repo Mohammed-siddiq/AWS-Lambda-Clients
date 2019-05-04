9b5e98e1c8d0d244616d3e9ac4782dacc2618c29
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-05-03 22:41:54 -0500

    Decoupled to make the methods unit testable

--- src/main/scala/calculator/RestCalculatorClient.scala
+++ src/main/scala/calculator/RestCalculatorClient.scala
@@ -1,21 +1,31 @@
 package calculator
 
-import scalaj.http.Http
+import scalaj.http.{Http, HttpResponse}
 
 /**
   * A Rest calculator client that creates a sample http GET request to the LAMBDA
+  *
+  * @param op1      : Operand 1
+  * @param op2      Operand 2
+  * @param operator Operator (+,-.*,/)
   */
-object RestCalculatorClient extends App {
-  println("Making Get Request .... ")
+class RestCalculatorClient(op1: Double, op2: Double, operator: String) extends App {
+
+  def runLambda(): HttpResponse[String] = {
+    println("Making Get Request .... ")
+
+    val result = Http(ConfigLoader.END_POINT).param(ConfigLoader.OPERAND_1, op1.toString).
+      param(ConfigLoader.OPERAND_2, op2.toString).
+      param(ConfigLoader.OPERATOR, operator)
+      .header(ConfigLoader.X_API_KEY_STR, ConfigLoader.X_API_KEY).asString
 
 
-  val result = Http(ConfigLoader.END_POINT).param("op1", "10.33").
-    param("op2", "30").
-    param("operator", "-")
-    .header(ConfigLoader.X_API_KEY_STR, ConfigLoader.X_API_KEY).asString
+    println("Response form the lambda function ")
+
+    println(result.body)
+    result
+
+  }
 
 
-  println("Response form the lambda function ")
-
-  println(result.body)
 }
