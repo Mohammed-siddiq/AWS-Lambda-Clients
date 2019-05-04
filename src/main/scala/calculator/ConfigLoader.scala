bf37ddbda67932b6120cc2c6002b755150720d68
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-05-03 22:42:09 -0500

    Decoupled to make the methods unit testable

--- src/main/scala/calculator/ConfigLoader.scala
+++ src/main/scala/calculator/ConfigLoader.scala
@@ -2,6 +2,9 @@ package calculator
 
 import com.typesafe.config.ConfigFactory
 
+/**
+  * A singleton config loader for the entire application
+  */
 object ConfigLoader {
   val conf = ConfigFactory.load("application")
   val X_API_KEY_STR = conf.getString("RAW_X_API_KEY")
@@ -10,4 +13,8 @@ object ConfigLoader {
 
   val CONTENT_TYPE_STR = conf.getString("CONTENT_TYPE_STR")
   val GRPC_CONTENT_TYPE = conf.getString("GRPC_CONTENT_TYPE")
+  val JSON_CONTENT_TYPE = conf.getString("JSON_CONTENT_TYPE")
+  val OPERAND_1 = conf.getString("OPERAND_1")
+  val OPERAND_2 = conf.getString("OPERAND_2")
+  val OPERATOR = conf.getString("OPERATOR")
 }
--- src/main/scala/calculator/RpcCalculatorClient.scala
+++ src/main/scala/calculator/RpcCalculatorClient.scala
@@ -2,36 +2,46 @@ package calculator
 
 
 import io.grpc.calculator.{Input, Response}
-import scalaj.http.{Base64, Http}
+import scalaj.http.{Base64, Http, HttpResponse}
 
 /**
   * A RPC rest client that creates a protobuffer binary request and reads the response as a protobuffer binary
+  *
+  * @param op1      operand 1
+  * @param op2      operand 2
+  * @param operator operator
   */
-object RpcCalculatorClient extends App {
-
-  println("Sending Request Proto .... ")
+class RpcCalculatorClient(op1: Double, op2: Double, operator: String) extends App {
 
 
-  //Creating protobufferr input message
-  val data = Input(500, 100, "+")
-
-  println(data.toString)
-
-  // Sending the protobuf binary array as a request to AWS lambda
-  val result = Http(ConfigLoader.END_POINT).postData(data.toByteArray)
-    .header(ConfigLoader.CONTENT_TYPE_STR, ConfigLoader.GRPC_CONTENT_TYPE)
-    .header(ConfigLoader.X_API_KEY_STR, ConfigLoader.X_API_KEY).asString
+  def runLambda(): Response = {
 
 
-  //Performing base64 decoding of the response proto
-  val raw_body = Base64.decode(result.body)
-
-  //constructing the response proto from the binary array
-  val returnedResponse = Response.parseFrom(raw_body)
-
-  println("Response form the lambda function ")
-
-  println(returnedResponse.toString)
+    println("Sending Request Proto .... ")
 
 
+    //Creating protobufferr input message
+    val data = Input(op1, op2, operator)
+
+    println(data.toString)
+
+    // Sending the protobuf binary array as a request to AWS lambda
+    val result = Http(ConfigLoader.END_POINT).postData(data.toByteArray)
+      .header(ConfigLoader.CONTENT_TYPE_STR, ConfigLoader.GRPC_CONTENT_TYPE)
+      .header(ConfigLoader.X_API_KEY_STR, ConfigLoader.X_API_KEY).asString
+
+
+    //Performing base64 decoding of the response proto
+    val raw_body = Base64.decode(result.body)
+
+    //constructing the response proto from the binary array
+    val returnedResponse = Response.parseFrom(raw_body)
+
+    println("Response form the lambda function ")
+
+    println(returnedResponse.toString)
+
+    returnedResponse
+
+  }
 }
